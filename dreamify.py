c59d81f515c4d8d0e3c7f4760aff005aa4d8ce66
himanshuxd <himanshuiris@gmail.com>
2023-11-29 10:18:33 +0530

    working version

--- dreamify.py
+++ dreamify.py
@@ -1,113 +1,141 @@
+import tkinter as tk
+from tkinter import filedialog
 import numpy as np
+import os
+import shutil
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras.applications import inception_v3
+from tensorflow.keras.preprocessing import image as keras_image
 
-base_image_path = keras.utils.get_file("sky.jpg", "https://i.imgur.com/aGBdQyK.jpg")
-result_prefix = "sky_dream"
-
-layer_settings = {
-    "mixed4": 1.0,
-    "mixed5": 1.5,
-    "mixed6": 2.0,
-    "mixed7": 2.5,
-}
-
-step = 0.01  # Gradient ascent step size
-num_octave = 3  # Number of scales at which to run gradient ascent
-octave_scale = 1.4  # Size ratio between scales
-iterations = 20  # Number of ascent steps per scale
-max_loss = 15.0
-
-from IPython.display import Image, display
-
-display(Image(base_image_path))
+file_path = ""
+result_path = ""
 
 def preprocess_image(image_path):
-    img = keras.preprocessing.image.load_img(image_path)
-    img = keras.preprocessing.image.img_to_array(img)
+    img = keras_image.load_img(image_path)
+    img = keras_image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     img = inception_v3.preprocess_input(img)
     return img
 
-
 def deprocess_image(x):
-    # Util function to convert a NumPy array into a valid image.
     x = x.reshape((x.shape[1], x.shape[2], 3))
-    # Undo inception v3 preprocessing
     x /= 2.0
     x += 0.5
     x *= 255.0
-    # Convert to uint8 and clip to the valid range [0, 255]
     x = np.clip(x, 0, 255).astype("uint8")
     return x
 
-model = inception_v3.InceptionV3(weights="imagenet", include_top=False)
+def deep_dream(image_path, result_path):
+    original_img = preprocess_image(image_path)
+    model = inception_v3.InceptionV3(weights="imagenet", include_top=False)
 
-outputs_dict = dict(
-    [
-        (layer.name, layer.output)
-        for layer in [model.get_layer(name) for name in layer_settings.keys()]
-    ]
-)
+    layer_settings = {"mixed4": 1.0, "mixed5": 1.5, "mixed6": 2.0, "mixed7": 2.5}
+    outputs_dict = {layer.name: layer.output for layer in [model.get_layer(name) for name in layer_settings.keys()]}
+    feature_extractor = tf.keras.Model(inputs=model.inputs, outputs=outputs_dict)
 
-feature_extractor = keras.Model(inputs=model.inputs, outputs=outputs_dict)
+    def compute_loss(input_image):
+        features = feature_extractor(input_image)
+        loss = tf.zeros(shape=())
+        for name in features.keys():
+            coeff = layer_settings[name]
+            activation = features[name]
+            scaling = tf.reduce_prod(tf.cast(tf.shape(activation), "float32"))
+            loss += coeff * tf.reduce_sum(tf.square(activation[:, 2:-2, 2:-2, :])) / scaling
+        return loss
 
-def compute_loss(input_image):
-    features = feature_extractor(input_image)
-    # Initialize the loss
-    loss = tf.zeros(shape=())
-    for name in features.keys():
-        coeff = layer_settings[name]
-        activation = features[name]
-        # We avoid border artifacts by only involving non-border pixels in the loss.
-        scaling = tf.reduce_prod(tf.cast(tf.shape(activation), "float32"))
-        loss += coeff * tf.reduce_sum(tf.square(activation[:, 2:-2, 2:-2, :])) / scaling
-    return loss
+    @tf.function
+    def gradient_ascent_step(img, learning_rate):
+        with tf.GradientTape() as tape:
+            tape.watch(img)
+            loss = compute_loss(img)
+        grads = tape.gradient(loss, img)
+        grads /= tf.maximum(tf.reduce_mean(tf.abs(grads)), 1e-6)
+        img += learning_rate * grads
+        return loss, img
 
-@tf.function
-def gradient_ascent_step(img, learning_rate):
-    with tf.GradientTape() as tape:
-        tape.watch(img)
-        loss = compute_loss(img)
-    grads = tape.gradient(loss, img)
-    grads /= tf.maximum(tf.reduce_mean(tf.abs(grads)), 1e-6)
-    img += learning_rate * grads
-    return loss, img
+    def gradient_ascent_loop(img, iterations, learning_rate, max_loss=None):
+        for _ in range(iterations):
+            loss, img = gradient_ascent_step(img, learning_rate)
+            if max_loss is not None and loss > max_loss:
+                break
+        return img
 
+    step = 0.01
+    iterations = 20
+    max_loss = 15.0
 
-def gradient_ascent_loop(img, iterations, learning_rate, max_loss=None):
-    for i in range(iterations):
-        loss, img = gradient_ascent_step(img, learning_rate)
-        if max_loss is not None and loss > max_loss:
-            break
-        print("... Loss value at step %d: %.2f" % (i, loss))
-    return img
+    for layer_name in layer_settings.keys():
+        img = tf.identity(original_img)
+        img = gradient_ascent_loop(img, iterations=iterations, learning_rate=step, max_loss=max_loss)
+        keras_image.save_img(result_path, deprocess_image(img.numpy()))
+    
+    root.destroy()
 
-original_img = preprocess_image(base_image_path)
-original_shape = original_img.shape[1:3]
+def open_file_dialog():
+    global file_path
+    global result_path
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png")])
+    if file_path:
+        result_path = file_path.split('.')[0] + '_dream.png'
+        deep_dream(file_path, result_path)
 
-successive_shapes = [original_shape]
-for i in range(1, num_octave):
-    shape = tuple([int(dim / (octave_scale ** i)) for dim in original_shape])
-    successive_shapes.append(shape)
-successive_shapes = successive_shapes[::-1]
-shrunk_original_img = tf.image.resize(original_img, successive_shapes[0])
+# GUI setup
+root = tk.Tk()
+root.title("Deep Dream GUI")
 
-img = tf.identity(original_img)  # Make a copy
-for i, shape in enumerate(successive_shapes):
-    print("Processing octave %d with shape %s" % (i, shape))
-    img = tf.image.resize(img, shape)
-    img = gradient_ascent_loop(
-        img, iterations=iterations, learning_rate=step, max_loss=max_loss
-    )
-    upscaled_shrunk_original_img = tf.image.resize(shrunk_original_img, shape)
-    same_size_original = tf.image.resize(original_img, shape)
-    lost_detail = same_size_original - upscaled_shrunk_original_img
+# Button to open file dialog
+btn_open_file = tk.Button(root, text="Open Image", command=open_file_dialog)
+btn_open_file.pack(pady=20)
 
-    img += lost_detail
-    shrunk_original_img = tf.image.resize(original_img, shape)
+root.mainloop()
 
-keras.preprocessing.image.save_img(result_prefix + ".png", deprocess_image(img.numpy()))
+print("File Path is : ", file_path)
+print("Result Path is : ", result_path)
 
-display(Image(result_prefix + ".png"))
+file_name = os.path.basename(file_path)
+result_name = os.path.basename(result_path)
+
+# Copy files to the current working directory
+shutil.copy(file_path, file_name)
+shutil.copy(result_path, result_name)
+
+# Generate HTML string with image paths
+html_content = f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Viewer</title>
+    <style>
+        body {{
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }}
+        img {{
+            max-width: 45%;
+            max-height: 80vh;
+        }}
+    </style>
+</head>
+<body>
+    <div>
+        <img src="{file_name}" alt="Original Image">
+    </div>
+    <div>
+        <img src="{result_name}" alt="Incepted Image">
+    </div>
+</body>
+</html>
+"""
+
+# Save the HTML string to a file
+with open("result_viewer.html", "w") as html_file:
+    html_file.write(html_content)
+
+# Open the generated HTML file in the default web browser
+import webbrowser
+webbrowser.open("result_viewer.html")
